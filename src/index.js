4fe4e8d4b621138e298b8a732f24be15ffa8ad3e
raju <rmaher84@gmail.com>
2020-04-07 09:00:02 +0200

    latest changes

--- src/index.js
+++ src/index.js
@@ -5,21 +5,55 @@ import * as tfvis from '@tensorflow/tfjs-vis';
 import factoringData from './datasets/factoring/factoring-data.json'
 console.log('>>>>>', factoringData);
 
+function onlyUnique(value, index, self) { 
+  return self.indexOf(value) === index;
+}
+
+const uniqueCustomerIds = factoringData
+  .map(record => record['customerID'])
+  .filter(onlyUnique);
+const uniqueDisputed = factoringData
+  .map(record => record['Disputed'])
+  .filter(onlyUnique);
+const uniquePaperlessBill = factoringData
+  .map(record => record['PaperlessBill'])
+  .filter(onlyUnique);
+
+const convertToFeatureVector = (
+  features
+) => {
+  return [
+    features['countryCode'],
+    uniqueCustomerIds.indexOf(features['customerID']),
+    (new Date(features['PaperlessDate']).getTime()),
+    features['invoiceNumber'],
+    (new Date(features['InvoiceDate']).getTime()),
+    (new Date(features['DueDate']).getTime()),
+    features['InvoiceAmount'],
+    uniqueDisputed.indexOf(features['Disputed']),
+    (new Date(features['SettledDate']).getTime()),
+    uniquePaperlessBill.indexOf(features['PaperlessBill']),
+    features['DaysToSettle'],
+  ];
+}
+
 const values = factoringData.map(({
-    InvoiceAmount: invoiceAmount,
-    DaysLate: daysLate
+  DaysLate: daysLate,
+  ...rest
 }) => ({
-    x: invoiceAmount,
+    x: convertToFeatureVector(rest),
     y: daysLate
-}))
+}));
+
+console.log('values', values)
 
 tfvis.visor();
 
 tfvis.render.scatterplot(
-    { name: 'InvoiceAmount v DaysLate' },
+    { name: 'features v DaysLate' },
     { values }, 
     {
-        xLabel: 'InvoiceAmount',
+        xLabel: 'features',
         yLabel: 'DaysLate',
         height: 300
     }
@@ -28,13 +62,21 @@ tfvis.render.scatterplot(
 // Create a sequential model
 const model = tf.sequential(); 
   
-// Add a single hidden layer
-model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
+// Add a single input layer
+model.add(tf.layers.dense({ units: 32, inputShape: [11], useBias: true, activation: 'relu'}));
+
+// Add a few hidden layers
+model.add(tf.layers.dense({ units: 128, activation: 'relu'}));
+model.add(tf.layers.dense({ units: 1024, activation: 'relu'}));
+model.add(tf.layers.dense({ units: 256, activation: 'relu'}));
+model.add(tf.layers.dense({ units: 64, activation: 'relu'}));
 
 // Add an output layer
-model.add(tf.layers.dense({units: 1, useBias: true}));
+model.add(tf.layers.dense({units: 1, useBias: true, activation: 'linear'}));
 
 tfvis.show.modelSummary({name: 'Model Summary'}, model);
+
+
 
 /**
  * Convert the input data to tensors that we can use for machine 
@@ -43,72 +85,83 @@ tfvis.show.modelSummary({name: 'Model Summary'}, model);
  * MPG on the y-axis.
  */
 function convertToTensor(data) {
-    // Wrapping these calculations in a tidy will dispose any 
-    // intermediate tensors.
-    
-    return tf.tidy(() => {
-      // Step 1. Shuffle the data    
-      tf.util.shuffle(data);
+  // Wrapping these calculations in a tidy will dispose any 
+  // intermediate tensors.
   
-      // Step 2. Convert data to Tensor
-      const formattedData = data.map(({ DaysLate, ...input}) => ({
-          input,
+  return tf.tidy(() => {
+    // Step 1. Shuffle the data    
+    tf.util.shuffle(data);
+
+    // Step 2. Convert data to Tensor
+    const formattedData = data.map(({ DaysLate, ...input}) => {
+      
+      return ({
+          input: convertToFeatureVector(input),
           label: DaysLate
-      }));
-
-      const inputs = formattedData.map(d => Object.keys(d.input).map(prop => ([prop, `${d.input[prop]}`.split('').map(Number)])))
-      const labels = formattedData.map(d => d.label);
-  
-      const inputTensor = tf.tensor3d(inputs, [inputs.length, inputs[0].length, 2]);
-      const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
-  
-      //Step 3. Normalize the data to the range 0 - 1 using min-max scaling
-      const inputMax = inputTensor.max();
-      const inputMin = inputTensor.min();  
-      const labelMax = labelTensor.max();
-      const labelMin = labelTensor.min();
-  
-      const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
-      const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
-  
-      return {
-        inputs: normalizedInputs,
-        labels: normalizedLabels,
-        // Return the min/max bounds so we can use them later.
-        inputMax,
-        inputMin,
-        labelMax,
-        labelMin,
-      }
-    });  
-  }
-
-  function trainModel(model, inputs, labels) {
-    // Prepare the model for training.  
-    model.compile({
-      optimizer: tf.train.adam(),
-      loss: tf.losses.meanSquaredError,
-      metrics: ['mse'],
+      });
     });
-    
-    const batchSize = 100;
-    const epochs = 100;
-    
-    return model.fit(inputs, labels, {
-      batchSize,
-      epochs,
-      shuffle: true,
-      callbacks: tfvis.show.fitCallbacks(
-        { name: 'Training Performance' },
-        ['loss', 'mse'], 
-        { height: 200, callbacks: ['onEpochEnd'] }
-      )
-    });
+
+    const inputs = formattedData.map(d => d.input);
+    const labels = formattedData.map(d => d.label);
+
+    const inputTensor = tf.tensor2d(inputs, [inputs.length, 11]);
+    const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+
+    //Step 3. Normalize the data to the range 0 - 1 using min-max scaling
+    const inputMax = inputTensor.max();
+    const inputMin = inputTensor.min();  
+    const labelMax = labelTensor.max();
+    const labelMin = labelTensor.min();
+
+    const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+    const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
+
+    return {
+      inputs: normalizedInputs,
+      labels: normalizedLabels,
+      // Return the min/max bounds so we can use them later.
+      inputMax,
+      inputMin,
+      labelMax,
+      labelMin,
+    }
+  });  
 }
 
 // Convert the data to a form we can use for training.
 const tensorData = convertToTensor(factoringData);
 const {inputs, labels} = tensorData;
+console.log('tensor created', inputs);
+
+function trainModel(model, inputs, labels) {
+  // Prepare the model for training.  
+  model.compile({
+    optimizer: tf.train.adam(),
+    loss: tf.losses.meanSquaredError,
+    metrics: ['mse'],
+  });
+
+  console.log('compiled model');
+  
+  const batchSize = 100;
+  const epochs = 100;
+
+  debugger;
+  
+  return model.fit(inputs, labels, {
+    batchSize,
+    epochs,
+    shuffle: true,
+    callbacks: (val) => {
+      console.log('val', val);
+      return tfvis.show.fitCallbacks(
+        { name: 'Training Performance' },
+        ['loss', 'mse'], 
+        { height: 200, callbacks: ['onEpochEnd'] }
+      );
+    }
+  });
+}
     
 // Train the model  
 trainModel(model, inputs, labels)
@@ -117,18 +170,20 @@ trainModel(model, inputs, labels)
 
         // test the model
         testModel(model, factoringData, tensorData)
-    });
+    })
 
 function testModel(model, inputData, normalizationData) {
-  const {inputMax, inputMin, labelMin, labelMax} = normalizationData;  
+  debugger;
+  const {inputMax, inputMin, labelMin, labelMax} = normalizationData;
   
   // Generate predictions for a uniform range of numbers between 0 and 1;
   // We un-normalize the data by doing the inverse of the min-max scaling 
   // that we did earlier.
   const [xs, preds] = tf.tidy(() => {
     
-    const xs = tf.linspace(0, 1, 100);      
-    const preds = model.predict(xs.reshape([100, 1]));      
+    const xs = tf.linspace(0, 11, 1);
+    console.log('xs', xs);
+    const preds = model.predict(xs.reshape([1, 11]));      
     
     const unNormXs = xs
       .mul(inputMax.sub(inputMin))
@@ -147,8 +202,9 @@ function testModel(model, inputData, normalizationData) {
     return {x: val, y: preds[i]}
   });
   
-  const originalPoints = inputData.map(d => ({
-    x: d.InvoiceAmount, y: d.DaysLate,
+  const originalPoints = inputData.map(({ DaysLate, ...rest }) => ({
+    x: convertToFeatureVector(rest),
+    y: d.DaysLate,
   }));
   
   
@@ -156,11 +212,9 @@ function testModel(model, inputData, normalizationData) {
     {name: 'Model Predictions vs Original Data'}, 
     {values: [originalPoints, predictedPoints], series: ['original', 'predicted']}, 
     {
-      xLabel: 'InvoiceAmount',
+      xLabel: 'rest',
       yLabel: 'DaysLate',
       height: 300
     }
   );
 }
-
-// testModel(model, factoringData, tensorData);
